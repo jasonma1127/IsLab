1203cb7ae602b2d709562456e085087aac9f6ed8
jasonma1127 <jasonma1127@gmail.com>
2022-11-01 13:38:17 +0900

    [NEW] 完成 avclass.py

---
+++ NICT/avclass/avclass.py
@@ -0,0 +1,69 @@
+import json
+import os
+import re
+from tqdm import tqdm
+
+def readJson(path: str) -> dict:
+    
+    with open(path, newline = '') as jsonfile:
+        data = json.load(jsonfile)
+
+    return data
+
+def store_new_result(data: dict) -> bool:
+    
+    json_object = json.dumps(data)
+
+    try:
+        with open("./fixed.json", "w") as outfile:
+            outfile.write(json_object)
+            return True
+    except:
+        return False
+
+def analysis_avclass_output(filename:str , path: str) -> dict:
+
+    with open(path, 'r') as f:
+        data = f.read()
+        
+    filename = filename.split(".")[0]
+
+    detail = re.split("\t", data)[-1]
+
+    detail = re.split("\|\d+,|\n|\|\d+", detail)
+
+    detail = [i for i in detail if i != '']
+
+    av_detail = dict()
+
+    for d in detail:
+        detailList = d.rsplit(":", 1)
+
+        try:
+            av_detail.update({detailList[0]: detailList[1]})
+        except:
+            print(filename, detail)
+
+    av = dict()
+    av.update({filename: av_detail})
+    # print(av)
+    return av
+
+if __name__ == "__main__":
+
+    total_avclass = dict()
+
+    for d in tqdm(sorted(os.listdir("/data/Results")), desc = "All"):
+        if len(d) == 2:
+            for f in tqdm(os.listdir("/data/Results/" + d), desc = d):
+                data = readJson("/data/Results/" + d + "/" + f)
+                store_new_result(data)
+                # perform avclass
+                os.system("~/avclass/avclass2/avclass2_labeler.py -vt ./fixed.json -p > /dev/null 2>&1 > avclassOutput.txt")
+                total_avclass.update(analysis_avclass_output(f, "./avclassOutput.txt"))  
+    
+    # store total avclass
+    with open("./new_total_avclass.json", "w") as outfile:
+        json.dump(total_avclass, outfile)
+
+    
