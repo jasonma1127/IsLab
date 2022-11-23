f80345cabe7751b65152a41f35edaadb75308113
jasonma1127 <jasonma1127@gmail.com>
2022-11-23 17:28:16 +0900

    [NEW] 新建 extractFeature.py 來呼叫 extractOpcode 的功能，並且用 import 並且用 import 與傳參的方式來呼叫

---
+++ NICT/extractOpcode/extractFeature.py
@@ -0,0 +1,19 @@
+from extractOpcode import extractOpcode
+import os, sys
+import argparse
+from tqdm import tqdm
+
+def get_parser():
+    parser = argparse.ArgumentParser(description = "Extract Features")
+    parser.add_argument("-i", "--input", type = str, help = "Enter source path")
+    parser.add_argument("-o", "--output", type = str, help = "Enter destination path")
+    return parser
+    
+if __name__ == "__main__":
+
+    parser = get_parser()
+    args = parser.parse_args()
+    # extractOpcode(args.input, args.output, "mal00ffff")
+
+    for file in tqdm(os.listdir(args.input), desc = "Extract Features"):
+        extractOpcode(args.input, args.output, file)
