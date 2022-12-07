d1f2f1cbc37b67240199945070db16bb463672d9
jasonma1127 <jasonma1127@gmail.com>
2022-12-07 20:50:51 +0900

    [UPDATE] Add extractOpcode_radare2.py to the function

--- NICT/extractOpcode/extractFeature.py
+++ NICT/extractOpcode/extractFeature.py
@@ -1,10 +1,12 @@
-from extractOpcode import extractOpcode
+from extractOpcode_retdec import extractOpcode_retdec
+from extractOpcode_radare2 import extractOpcode_radare2
 import os, sys
 import argparse
 from tqdm import tqdm
 
 def get_parser():
     parser = argparse.ArgumentParser(description = "Extract Features")
+    parser.add_argument("-t", "--tool", type = str, help = "Select tool you want to use")
     parser.add_argument("-i", "--input", type = str, help = "Enter source path")
     parser.add_argument("-o", "--output", type = str, help = "Enter destination path")
     return parser
@@ -14,5 +16,10 @@ if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
 
-    for file in tqdm(os.listdir(args.input), desc = "Extract Features"):
-        extractOpcode(args.input, args.output, file)
+    if args.tool == "retdec":
+        for file in tqdm(os.listdir(args.input), desc = "Extract Features using retdec"):
+            extractOpcode_retdec(args.input, args.output, file)
+    
+    if args.tool == "radare2":
+        for file in tqdm(os.listdir(args.input), desc = "Extract Features using radare2"):
+            extractOpcode_radare2(args.input, args.output, file)
